6db080d3c5556d70c80824fbd34e4f57e0135191
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2024-06-06 20:28:38 +0530

    fixed to update Constants' VERSION and CHANGES

--- src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
+++ src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
@@ -5,7 +5,8 @@ import org.slf4j.LoggerFactory;
 
 public class Constants
 {
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
+    public static final String CHANGES = "Add packetevents, new ingamehub, fixed gitignore, reset everything idk sorry for this desc future me"; // same as the commit name/description
     public static final String MOD_ID = "paradiseclient_fabric";
     public static final String MOD_NAME = "ParadiseClient-Fabric";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);

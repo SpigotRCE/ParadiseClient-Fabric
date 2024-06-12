bef8096a5761bd94718bedb07f90e8d9ea0a98e3
Milkthedev <pranavlal896@gmail.com>
2024-06-12 16:50:18 +0530

    Added ChatRoom support

--- src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
+++ src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 
 public class Constants
 {
-    public static final String VERSION = "1.4";
-    public static final String CHANGES = "Reorganised mods, Added new commands, etc."; // same as the commit name/description
+    public static final String VERSION = "1.4.1";
+    public static final String CHANGES = "ChatRoom username fix"; // same as the commit name/description
     public static final String MOD_ID = "paradiseclient_fabric";
     public static final String MOD_NAME = "ParadiseClient-Fabric";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);

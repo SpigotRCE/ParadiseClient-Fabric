7d1ca94f0bf97228a087e519c4c3868703966cbb
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2024-06-18 22:55:31 +0530

    Minor typo fix in

--- src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
+++ src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 
 public class Constants
 {
-    public static final String VERSION = "1.7";
-    public static final String CHANGES = "Add cmi forceop command"; // same as the commit name/description
+    public static final String VERSION = "1.8";
+    public static final String CHANGES = "Minor typo fix in "; // same as the commit name/description
     public static final String MOD_ID = "paradiseclient-fabric";
     public static final String MOD_NAME = "ParadiseClient-Fabric";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
--- src/main/java/tk/milkthedev/paradiseclientfabric/mixin/inject/InGameHudMixin.java
+++ src/main/java/tk/milkthedev/paradiseclientfabric/mixin/inject/InGameHudMixin.java
@@ -55,9 +55,9 @@ public abstract class InGameHudMixin
         text.add("ParadiseClient by SpigotRCE#0");
         text.add("Server " + ((!Objects.isNull(this.client.getCurrentServerEntry()) && ParadiseClient_Fabric.getHudMod().showServerIP) ? this.client.getCurrentServerEntry().address : "Hidden"));
         text.add("Engine " + (Objects.isNull(this.client.getNetworkHandler()) ? "" : this.client.getNetworkHandler().getBrand()));
-        text.add("Last Incoming Packet " + (System.currentTimeMillis() - miscMod.lastIncomingPacketTime) + "ms Average " + miscMod.averageIncomingPacketDelay + " ms");
+        text.add("Last Incoming Packet " + (System.currentTimeMillis() - miscMod.lastIncomingPacketTime) + "ms Average " + miscMod.averageIncomingPacketDelay + "ms");
         text.add("Packet " + miscMod.lastIncomingPacket.getPacketId().id());
-        text.add("Last Outgoing Packet " + (System.currentTimeMillis() - miscMod.lastOutgoingPacketTime) + "ms Average " + miscMod.averageOutgoingPacketDelay + " ms");
+        text.add("Last Outgoing Packet " + (System.currentTimeMillis() - miscMod.lastOutgoingPacketTime) + "ms Average " + miscMod.averageOutgoingPacketDelay + "ms");
         text.add("Packet " + miscMod.lastOutgoingPacket.getPacketId().id());
         text.add("FPS " + this.client.getCurrentFps());
 

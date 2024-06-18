4b8d6670981c370d9ad6b2d811826af577730d64
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2024-06-18 14:54:37 +0530

    Add cmi forceop command

--- src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
+++ src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
@@ -5,9 +5,9 @@ import org.slf4j.LoggerFactory;
 
 public class Constants
 {
-    public static final String VERSION = "1.6";
-    public static final String CHANGES = "Add completioncrashexploit and command"; // same as the commit name/description
-    public static final String MOD_ID = "paradiseclient_fabric";
+    public static final String VERSION = "1.7";
+    public static final String CHANGES = "Add cmi forceop command"; // same as the commit name/description
+    public static final String MOD_ID = "paradiseclient-fabric";
     public static final String MOD_NAME = "ParadiseClient-Fabric";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 }

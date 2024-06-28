b02587ea0c60a61475b94a8542991226d75076f3
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-06-28 19:13:32 +0530

    Updated to version Minecraft version 1.21

--- src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
+++ src/main/java/tk/milkthedev/paradiseclientfabric/Constants.java
@@ -5,8 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class Constants
 {
-    public static final String VERSION = "1.10";
-    public static final String CHANGES = "Added fake hostname spoofing to bypass forced host"; // same as the commit name/description
+    public static final String VERSION = "2.0";
     public static final String MOD_ID = "paradiseclient-fabric";
     public static final String MOD_NAME = "ParadiseClient-Fabric";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);

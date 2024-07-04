bf07529fe9049827310e70c97d72b68bd178f471
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-07-04 20:12:29 +0530

    Add copy cmd & wallpaper to more screens

--- src/main/java/tk/milkthedev/paradiseclientfabric/mixin/inject/ScreenMixin.java
+++ src/main/java/tk/milkthedev/paradiseclientfabric/mixin/inject/ScreenMixin.java
@@ -2,8 +2,11 @@ package tk.milkthedev.paradiseclientfabric.mixin.inject;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.screen.DisconnectedScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.multiplayer.AddServerScreen;
+import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
+import net.minecraft.client.gui.screen.multiplayer.DirectConnectScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,11 +24,15 @@ public abstract class ScreenMixin
     @Shadow public int width;
     @Unique
     private final Identifier backgroundImage = Identifier.of(Constants.MOD_ID, "wallpaper.png");
+    @Unique
+    private Screen screen;
 
     @Inject(method = "renderBackground", at = @At(value = "HEAD"), cancellable = true)
     private void renderBackground(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci)
     {
-        if (MinecraftClient.getInstance().currentScreen instanceof MultiplayerScreen)
+        screen = MinecraftClient.getInstance().currentScreen;
+        if (screen instanceof MultiplayerScreen || screen instanceof DisconnectedScreen || screen instanceof AddServerScreen
+            ||screen instanceof DirectConnectScreen || screen instanceof ConnectScreen)
         {
             context.drawTexture(backgroundImage, 0, 0, this.width, this.height, 0.0F, 0.0F, this.width, this.height, this.width, this.height);
             ci.cancel();

28ac214909c100fc459e84240de02ea7c6f7b493
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-07-05 16:47:13 +0530

    Better exploits manager and some imports cleaned

--- src/main/java/tk/milkthedev/paradiseclientfabric/ParadiseClient_Fabric.java
+++ src/main/java/tk/milkthedev/paradiseclientfabric/ParadiseClient_Fabric.java
@@ -3,6 +3,9 @@ package tk.milkthedev.paradiseclientfabric;
 import net.fabricmc.api.ModInitializer;
 import tk.milkthedev.paradiseclientfabric.command.CommandManager;
 import tk.milkthedev.paradiseclientfabric.command.impl.*;
+import tk.milkthedev.paradiseclientfabric.exploit.ExploitManager;
+import tk.milkthedev.paradiseclientfabric.exploit.impl.BrigadierExploit;
+import tk.milkthedev.paradiseclientfabric.exploit.impl.PaperWindowExploit;
 import tk.milkthedev.paradiseclientfabric.mod.BungeeSpoofMod;
 import tk.milkthedev.paradiseclientfabric.mod.ChatRoomMod;
 import tk.milkthedev.paradiseclientfabric.mod.HudMod;
@@ -14,12 +17,14 @@ public class ParadiseClient_Fabric implements ModInitializer
     private static MiscMod miscMod;
     private static HudMod hudMod;
     private static CommandManager commandManager;
+    private static ExploitManager exploitManager;
     private static ChatRoomMod chatRoomMod;
 
     public static BungeeSpoofMod getBungeeSpoofMod() {return bungeeSpoofMod;}
     public static MiscMod getMiscMod() {return miscMod;}
     public static HudMod getHudMod() {return hudMod;}
     public static CommandManager getCommandManager() {return commandManager;}
+    public static ExploitManager getExploitManager() {return exploitManager;}
     public static ChatRoomMod getChatRoomMod() {return chatRoomMod;}
 
     @Override
@@ -39,6 +44,10 @@ public class ParadiseClient_Fabric implements ModInitializer
                 new CrashCommand(),
                 new ForceOPCommand(),
                 new CopyCommand()
+        );
+        exploitManager = new ExploitManager(
+                new BrigadierExploit(),
+                new PaperWindowExploit()
         );
     }
 }

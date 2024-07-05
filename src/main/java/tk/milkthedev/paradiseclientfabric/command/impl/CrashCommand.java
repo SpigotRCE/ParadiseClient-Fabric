3daa2c842dc14a125bea8c851250ec4c83d9a171
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-07-05 16:47:13 +0530

    Better exploits manager and some imports cleaned

--- src/main/java/tk/milkthedev/paradiseclientfabric/command/impl/CrashCommand.java
+++ src/main/java/tk/milkthedev/paradiseclientfabric/command/impl/CrashCommand.java
@@ -1,13 +1,18 @@
 package tk.milkthedev.paradiseclientfabric.command.impl;
 
+import tk.milkthedev.paradiseclientfabric.Helper;
+import tk.milkthedev.paradiseclientfabric.ParadiseClient_Fabric;
 import tk.milkthedev.paradiseclientfabric.command.Command;
 import tk.milkthedev.paradiseclientfabric.command.CommandInfo;
 import tk.milkthedev.paradiseclientfabric.command.exception.CommandException;
-import tk.milkthedev.paradiseclientfabric.exploit.CompletionCrashExploit;
+import tk.milkthedev.paradiseclientfabric.exploit.Exploit;
+import tk.milkthedev.paradiseclientfabric.exploit.impl.BrigadierExploit;
+
+import java.util.Objects;
 
 @CommandInfo(
         alias = "crash",
-        description = "Crashes the server. As of now, crashes through the StackOverFlow crash exploit",
+        description = "Crashes the server.",
         usage = "crash <method|list>"
 )
 public class CrashCommand extends Command
@@ -15,7 +20,16 @@ public class CrashCommand extends Command
     @Override
     public boolean execute(String commandAlias, String... args) throws CommandException
     {
-        new CompletionCrashExploit().enable();
+        if (args.length != 1) return false;
+
+        if (Objects.equals(args[0], "list"))
+        {
+            Helper.printChatMessage("Available commands:");
+            for (Exploit exploit : ParadiseClient_Fabric.getExploitManager().getExploits()) {Helper.printChatMessage(exploit.getAlias() + " " + exploit.getDescription());}
+            return true;
+        }
+
+        ParadiseClient_Fabric.getExploitManager().handleExploit(args[0]);
         return true;
     }
 

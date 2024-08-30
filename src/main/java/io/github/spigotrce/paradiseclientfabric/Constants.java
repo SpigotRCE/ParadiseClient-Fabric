559fb8aa0354ca43574a7328ace07fa0328f5a48
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-30 21:32:14 +0530

    added background to all screens

--- src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
@@ -1,11 +1,14 @@
 package io.github.spigotrce.paradiseclientfabric;
 
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Constants {
-    public static final String VERSION = "2.19";
+    public static final String VERSION = "2.20";
     public static final String MOD_ID = "paradiseclient-fabric";
     public static final String MOD_NAME = "ParadiseClient-Fabric";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+    public static final Identifier backgroundImage = Identifier.of(Constants.MOD_ID, "wallpaper.png");
 }

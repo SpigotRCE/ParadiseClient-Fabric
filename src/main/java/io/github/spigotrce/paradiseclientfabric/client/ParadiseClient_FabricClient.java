e27a980d44fe68e746ac4d483c67fe829c798d3e
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-31 13:46:28 +0530

    added java doc

--- src/main/java/io/github/spigotrce/paradiseclientfabric/client/ParadiseClient_FabricClient.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/client/ParadiseClient_FabricClient.java
@@ -2,8 +2,27 @@ package io.github.spigotrce.paradiseclientfabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
+/**
+ * This class is the entry point for the client-side functionality of the ParadiseClient mod.
+ * It implements the {@link ClientModInitializer} interface, which is a FabricMC API for initializing client-side mods.
+ *
+ * <p>The {@link #onInitializeClient()} method is called when the client-side mod is initialized.
+ * This method is overridden from the {@link ClientModInitializer} interface and should contain any necessary client-side initialization logic.
+ *
+ * @author SpigotRCE
+ * @since 1.0
+ */
 public class ParadiseClient_FabricClient implements ClientModInitializer {
+    /**
+     * Initializes the client-side functionality of the ParadiseClient mod.
+     * This method is called when the client-side mod is loaded.
+     *
+     * <p>No parameters are required for this method.
+     *
+     * <p>The return value is void, as there is no meaningful result to return from this method.
+     */
     @Override
     public void onInitializeClient() {
+        // Add your client-side initialization logic here
     }
 }

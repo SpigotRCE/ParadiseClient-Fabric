f90aa40523074705fdb86537249bd213082721e7
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-31 11:19:17 +0530

    removed unused packets

--- src/main/java/io/github/spigotrce/paradiseclientfabric/client/ParadiseClient_FabricClient.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/client/ParadiseClient_FabricClient.java
@@ -1,16 +1,9 @@
 package io.github.spigotrce.paradiseclientfabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import io.github.spigotrce.paradiseclientfabric.packet.AuthMeVelocityPayloadPacket;
-import io.github.spigotrce.paradiseclientfabric.packet.ChatSentryPayloadPacket;
 
 public class ParadiseClient_FabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        PayloadTypeRegistry.playC2S().register(ChatSentryPayloadPacket.ID, ChatSentryPayloadPacket.CODEC);
-        PayloadTypeRegistry.playS2C().register(ChatSentryPayloadPacket.ID, ChatSentryPayloadPacket.CODEC);
-        PayloadTypeRegistry.playC2S().register(AuthMeVelocityPayloadPacket.ID, AuthMeVelocityPayloadPacket.CODEC);
-        PayloadTypeRegistry.playS2C().register(AuthMeVelocityPayloadPacket.ID, AuthMeVelocityPayloadPacket.CODEC);
     }
 }

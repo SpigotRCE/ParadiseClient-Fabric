59d7d574de11afbdebdb34e17899116d2850261b
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-31 13:46:16 +0530

    added java doc

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/ScreenShareCommand.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/ScreenShareCommand.java
@@ -7,11 +7,28 @@ import io.github.spigotrce.paradiseclientfabric.ParadiseClient_Fabric;
 import io.github.spigotrce.paradiseclientfabric.command.Command;
 import net.minecraft.client.MinecraftClient;
 
+/**
+ * Represents a command that toggles the display of server IP on the HUD.
+ *
+ * @author SpigotRCE
+ * @since 1.4
+ */
 public class ScreenShareCommand extends Command {
+
+    /**
+     * Constructs a new instance of {@link ScreenShareCommand}.
+     *
+     * @param minecraftClient The Minecraft client instance.
+     */
     public ScreenShareCommand(MinecraftClient minecraftClient) {
         super("paradisescreenshare", "Toggles IP displayed on HUD", minecraftClient);
     }
 
+    /**
+     * Builds the command structure using Brigadier's {@link LiteralArgumentBuilder}.
+     *
+     * @return The built command structure.
+     */
     @Override
     public LiteralArgumentBuilder<FabricClientCommandSource> build() {
         return literal(getName()).executes((context -> {

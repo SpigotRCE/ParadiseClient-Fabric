7085472ac4fd7687ee804337628437f132f99c03
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-31 13:47:13 +0530

    added java doc

--- src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/CopyCommand.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/command/impl/CopyCommand.java
@@ -10,17 +10,37 @@ import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 
+/**
+ * This class represents a command that copies a specific broadcast message to the clipboard.
+ * The command is part of the ParadiseClientFabric mod for Minecraft.
+ *
+ * @author SpigotRCE
+ * @version 1.0
+ */
 public class CopyCommand extends Command {
+
+    /**
+     * Constructs a new CopyCommand instance.
+     *
+     * @param minecraftClient The Minecraft client instance.
+     */
     public CopyCommand(MinecraftClient minecraftClient) {
         super("paradisecopy", "Copies the broadcast of SpigotRCE", minecraftClient);
     }
 
+    /**
+     * Builds the command structure using Brigadier's LiteralArgumentBuilder.
+     * The command has two sub-commands: "tellraw" and the default command.
+     *
+     * @return The built command structure.
+     */
     @Override
     public LiteralArgumentBuilder<FabricClientCommandSource> build() {
         return
                 literal(getName())
                         .then(literal("tellraw")
                                 .executes((context) -> {
+                                    // Copies a specific tellraw message to the clipboard.
                                     StringSelection stringSelection = new StringSelection("tellraw @a [{\"text\":\"Server hacked by\\n\", \"color\":\"green\"},{\"text\":\"https://youtube.com/@SpigotRCE\", \"color\":\"aqua\", \"bold\":true, \"clickEvent\":{\"action\":\"open_url\",\"value\":\"https://youtube.com/@SpigotRCE\"}}]");
                                     Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
                                     clipboard.setContents(stringSelection, null);
@@ -28,6 +48,7 @@ public class CopyCommand extends Command {
                                     return SINGLE_SUCCESS;
                                 }))
                         .executes((context) -> {
+                            // Copies a specific formatted message to the clipboard.
                             StringSelection stringSelection = new StringSelection("&aServer hacked by&b&l https://youtube.com/@SpigotRCE");
                             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
                             clipboard.setContents(stringSelection, null);

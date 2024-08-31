7a561d600bf56a69b714609711407d743aefeb8b
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-31 13:47:17 +0530

    added java doc

--- src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
@@ -1,3 +1,10 @@
+/**
+ * This class contains various constants used throughout the ParadiseClient-Fabric mod.
+ *
+ * @author SpigotRCE
+ * @version 2.22
+ * @since 1.0
+ */
 package io.github.spigotrce.paradiseclientfabric;
 
 import net.minecraft.util.Identifier;
@@ -5,12 +12,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Constants {
+    /**
+     * The version of the mod.
+     */
     public static final String VERSION = "2.22";
+
+    /**
+     * The ID of the mod.
+     */
     public static final String MOD_ID = "paradiseclient-fabric";
+
+    /**
+     * The name of the mod.
+     */
     public static final String MOD_NAME = "ParadiseClient-Fabric";
+
+    /**
+     * The logger for the mod.
+     */
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
+    /**
+     * The identifier for the background image.
+     */
     public static final Identifier backgroundImage = Identifier.of(Constants.MOD_ID, "wallpaper.png");
 
+    /**
+     * The edition of the mod.
+     */
     public static String EDITION = "PUBLIC"; // For API
 }

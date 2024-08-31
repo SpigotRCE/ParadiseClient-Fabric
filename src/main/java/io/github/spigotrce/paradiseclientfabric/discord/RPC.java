e17d42b647769332197fce39b996e96a9b5db451
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-31 13:46:20 +0530

    added java doc

--- src/main/java/io/github/spigotrce/paradiseclientfabric/discord/RPC.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/discord/RPC.java
@@ -15,21 +15,32 @@ import java.io.InputStream;
 import java.time.Instant;
 import java.util.Objects;
 
+/**
+ * Implements Runnable interface to run Discord Rich Presence functionality.
+ * This class sets up the Discord RPC client, updates the presence status, and handles the game's state.
+ *
+ * @author SpigotRCE
+ * @since 2.17
+ */
 public class RPC implements Runnable {
     @Override
     public void run() {
         this.loadDiscordRPC();
 
+        // Initialize Discord Game SDK with the DLL file
         Core.init(new File(MinecraftClient.getInstance().runDirectory.getAbsolutePath() + "\\paradise\\discord\\discord_game_sdk.dll"));
 
         try (CreateParams params = new CreateParams()) {
+            // Set the client ID for the Discord application
             params.setClientID(1164104022265974784L);
             params.setFlags(CreateParams.getDefaultFlags());
 
             try (Core core = new Core(params)) {
                 try (Activity activity = new Activity()) {
+                    // Set initial activity details and timestamp
                     activity.setDetails("In Menu");
                     activity.timestamps().setStart(Instant.now());
+                    // Uncomment to set large image
 //                    activity.assets().setLargeImage(DiscordPresenceConstants.IMAGE);
                     core.activityManager().updateActivity(activity);
 
@@ -37,10 +48,12 @@ public class RPC implements Runnable {
                     while (true) {
                         core.runCallbacks();
                         try {
+                            // Update activity details and state based on game state
                             if (ParadiseClient_Fabric.getNetworkMod().isConnected) {
                                 activity.setDetails("Playing on a server");
                                 activity.setState(((!Objects.isNull(MinecraftClient.getInstance().getCurrentServerEntry()) && ParadiseClient_Fabric.getHudMod().showServerIP) ? MinecraftClient.getInstance().getCurrentServerEntry().address : "Hidden"));
                             } else {
+                                // Uncomment to set activity details based on current screen
 //                                activity.setDetails(Objects.isNull(ParadiseClient_Fabric.getMiscMod().currentScreen) ? "In Menu" : Objects.isNull(ParadiseClient_Fabric.getMiscMod().currentScreen.getTitle()) ? "In Menu" : ParadiseClient_Fabric.getMiscMod().currentScreen.getTitle().getLiteralString());
                                 activity.setDetails("In Menu");
                                 activity.setState("");
@@ -57,6 +70,9 @@ public class RPC implements Runnable {
         }
     }
 
+    /**
+     * Loads the Discord Game SDK DLL file from the resource manager and copies it to the game directory.
+     */
     private void loadDiscordRPC() {
         Identifier identifier = Identifier.of(Constants.MOD_ID, "discord/discord_game_sdk.dll");
 

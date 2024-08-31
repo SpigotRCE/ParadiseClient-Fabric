5af265f5ae8b2d3f936832b06f37d7816fc3d784
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-31 14:22:43 +0530

    more java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/mixin/accessor/SessionAccessor.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/mixin/accessor/SessionAccessor.java
@@ -1,5 +1,19 @@
 package io.github.spigotrce.paradiseclientfabric.mixin.accessor;
 
+/**
+ * This interface provides an accessor method for setting the username in a session.
+ * It is used in the Paradise Client Fabric mod to modify the username before it is sent to the server.
+ *
+ * @author Spigotrce
+ * @since 1.0
+ */
 public interface SessionAccessor {
+
+    /**
+     * Sets the username in the session.
+     *
+     * @param username The new username to be set. This parameter should not be null or empty.
+     *                 The username should be a valid string according to Minecraft's username requirements.
+     */
     void paradiseClient_Fabric$setUsername(String username);
 }

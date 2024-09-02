9153c71341b0ec23da68924c0914b897da87dd58
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-09-02 15:33:03 +0530

    removed packets from hud

--- src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     /**
      * The version of the mod.
      */
-    public static final String VERSION = "2.25";
+    public static final String VERSION = "2.26";
 
     /**
      * The ID of the mod.

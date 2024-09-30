45a8e7e0d3ba5ab914b5a2b11bbb1d13e8525916
SpigotRCE <spigotrce@gmail.com>
2024-09-30 15:05:27 +0530

    changed message in exploit unknown and some api changes

--- src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     /**
      * The version of the mod.
      */
-    public static final String VERSION = "1.21-3-2";
+    public static final String VERSION = "1.21-3-3";
 
     /**
      * The ID of the mod.

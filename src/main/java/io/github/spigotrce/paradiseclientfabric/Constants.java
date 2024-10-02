90db967e27ab3f0fab246c3c8db5aa899ca76da1
SpigotRCE <spigotrce@gmail.com>
2024-10-02 21:06:47 +0530

    removed unused slotcrash

--- src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     /**
      * The version of the mod.
      */
-    public static final String VERSION = "1.21-3-3";
+    public static final String VERSION = "1.21-3-4";
 
     /**
      * The ID of the mod.

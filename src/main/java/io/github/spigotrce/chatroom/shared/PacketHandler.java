090d458314778a6d467dc8f2728de07bbc2a3260
Zffu <radi0o0@outlook.com>
2024-10-13 22:36:31 +0200

    feat: added O param for later

--- src/main/java/io/github/spigotrce/chatroom/shared/PacketHandler.java
+++ src/main/java/io/github/spigotrce/chatroom/shared/PacketHandler.java
@@ -1,15 +1,17 @@
 package io.github.spigotrce.chatroom.shared;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * <p>Handles the packet's behaviors.</p>
  */
-public interface PacketHandler {
+public interface PacketHandler<O> {
 
-    default void handle() {
+    default void handle(@Nullable O o) {
         throw new IllegalStateException("The handler for the current protocol direction doesn't handle reading packets!");
     }
 
-    default void write() {
+    default void write(@Nullable O o) {
         throw new IllegalStateException("The handler for the current protocol direction doesn't handle writing packets!");
     }
 

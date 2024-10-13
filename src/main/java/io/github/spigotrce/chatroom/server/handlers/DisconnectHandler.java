a4029bdeac3d936ce5b06e1aa763747a7aefcfda
Zffu <radi0o0@outlook.com>
2024-10-13 22:51:35 +0200

    fix: disconnect handler

--- src/main/java/io/github/spigotrce/chatroom/server/handlers/DisconnectHandler.java
+++ src/main/java/io/github/spigotrce/chatroom/server/handlers/DisconnectHandler.java
@@ -1,14 +1,15 @@
 package io.github.spigotrce.chatroom.server.handlers;
 
+import io.github.spigotrce.chatroom.server.ServerShard;
 import io.github.spigotrce.chatroom.shared.PacketHandler;
 
 /**
  * <p>The client handler for the disconnect packet.</p>
  */
-public class DisconnectHandler implements PacketHandler<Object> {
+public class DisconnectHandler implements PacketHandler<ServerShard> {
 
     @Override
-    public void handle(Object o) {
+    public void handle(ServerShard o) {
         System.out.println("Client disconnected!");
     }
 }

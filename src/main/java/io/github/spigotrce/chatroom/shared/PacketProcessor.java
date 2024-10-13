5141127565d53c929536b70495da0c5f0e1c7c7e
Zffu <radi0o0@outlook.com>
2024-10-13 12:26:45 +0200

    feat: added packet processor base

---
+++ src/main/java/io/github/spigotrce/chatroom/shared/PacketProcessor.java
@@ -0,0 +1,18 @@
+package io.github.spigotrce.chatroom.shared;
+
+/**
+ * <p>The base of the chatroom client & server.</p>
+ */
+public class PacketProcessor<K extends Enum<?>> {
+
+    private DoubleSidedEnumRegistry<PacketType, K> registry;
+
+    /**
+     * <p>Creates a new packet processor.</p>
+     * @param handlers the handlers.
+     */
+    public PacketProcessor(K[] handlers) {
+        this.registry = new DoubleSidedEnumRegistry<>(PacketType.values(), handlers);
+    }
+
+}

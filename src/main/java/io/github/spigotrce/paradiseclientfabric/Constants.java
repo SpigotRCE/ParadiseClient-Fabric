82caa02d2d82b5017fa78e5e2b0844ee5ecdeeca
SpigotRCE <spigotrce@gmail.com>
2024-10-20 22:28:50 +0530

    added keybinding for command prefix

--- src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     /**
      * The version of the mod.
      */
-    public static final String VERSION = "1.21-3-5";
+    public static final String VERSION = "1.21-3-6";
 
     /**
      * The ID of the mod.

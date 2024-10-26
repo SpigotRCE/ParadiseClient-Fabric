69e56fae4a60be6249e9bf1580bd67e85daf9745
SpigotRCE <spigotrce@gmail.com>
2024-10-26 23:48:35 +0530

    made matrix persistent

--- src/main/java/io/github/spigotrce/paradiseclientfabric/mixin/inject/gui/screen/ScreenMixin.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/mixin/inject/gui/screen/ScreenMixin.java
@@ -1,5 +1,6 @@
 package io.github.spigotrce.paradiseclientfabric.mixin.inject.gui.screen;
 
+import io.github.spigotrce.paradiseclientfabric.WallPaper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
@@ -24,11 +25,6 @@ public abstract class ScreenMixin {
     @Shadow public int height;
     @Shadow public int width;
 
-    @Shadow protected MinecraftClient client;
-    @Unique private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    @Unique private final int[] drops = new int[300];
-    @Unique private final Random random = new Random();
-
     /**
      * Injects custom background rendering into the renderBackground method.
      * This method draws a custom texture for specific screens and cancels the original rendering.
@@ -41,15 +37,7 @@ public abstract class ScreenMixin {
      */
     @Inject(method = "renderBackground", at = @At(value = "HEAD"), cancellable = true)
     private void renderBackground(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        context.fillGradient(0, 0, width, height, 0xCC000000, 0xCC000000);
-        for (int i = 0; i < drops.length; i++) {
-            String text = String.valueOf(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
-            context.drawText(this.client.textRenderer, text, i * 10, drops[i] * 10, 0x00FF00, false);
-
-            if (drops[i] * 10 > height && random.nextDouble() > 0.975)
-                drops[i] = 0;
-            drops[i]++;
-        }
+        WallPaper.renderMatrix(context, width, height);
         ci.cancel();
     }
 }

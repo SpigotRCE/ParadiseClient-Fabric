259b9ffab29b21645ec3858b8ffeca232bb77287
SpigotRCE <spigotrce@gmail.com>
2024-11-01 21:49:06 +0530

    code cleanup

--- src/main/java/io/github/spigotrce/paradiseclientfabric/mixin/inject/gui/RotatingCubeMapRendererMixin.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/mixin/inject/gui/RotatingCubeMapRendererMixin.java
@@ -1,27 +1,15 @@
 package io.github.spigotrce.paradiseclientfabric.mixin.inject.gui;
 
-import io.github.spigotrce.paradiseclientfabric.Helper;
 import io.github.spigotrce.paradiseclientfabric.WallPaper;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.RotatingCubeMapRenderer;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Random;
-
 @Mixin(RotatingCubeMapRenderer.class)
 public class RotatingCubeMapRendererMixin {
-    @Shadow @Final private MinecraftClient client;
-    @Unique private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    @Unique private final int[] drops = new int[300];
-    @Unique private final Random random = new Random();
-
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     public void render(DrawContext context, int width, int height, float alpha, float tickDelta, CallbackInfo ci) {
         WallPaper.renderMatrix(context, width, height);

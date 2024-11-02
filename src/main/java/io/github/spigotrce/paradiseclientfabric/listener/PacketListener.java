dc9e4bd0c2939109b431bab502992ac44337b058
SpigotRCE <spigotrce@gmail.com>
2024-11-02 21:02:12 +0530

    added texture pack grabber

--- src/main/java/io/github/spigotrce/paradiseclientfabric/listener/PacketListener.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/listener/PacketListener.java
@@ -2,16 +2,21 @@ package io.github.spigotrce.paradiseclientfabric.listener;
 
 import io.github.spigotrce.eventbus.event.EventHandler;
 import io.github.spigotrce.eventbus.event.listener.Listener;
+import io.github.spigotrce.paradiseclientfabric.Helper;
 import io.github.spigotrce.paradiseclientfabric.ParadiseClient_Fabric;
 import io.github.spigotrce.paradiseclientfabric.event.packet.incoming.PacketIncomingPostEvent;
+import io.github.spigotrce.paradiseclientfabric.event.packet.incoming.PacketIncomingPreEvent;
 import io.github.spigotrce.paradiseclientfabric.event.packet.outgoing.PacketOutgoingPostEvent;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.s2c.common.ResourcePackSendS2CPacket;
 
 @SuppressWarnings("unused")
 public class PacketListener implements Listener {
     @EventHandler
-    public void onIncomingPacketReceive(PacketIncomingPostEvent event) {
-        ParadiseClient_Fabric.getNetworkMod().lastIncomingPacket = event.getPacket();
-        ParadiseClient_Fabric.getNetworkMod().lastIncomingPacketTime = System.currentTimeMillis();
+    public void onResourcePackPacketReceive(PacketIncomingPreEvent event) {
+        if (!(event.getPacket() instanceof ResourcePackSendS2CPacket packet)) return;
+        String url = packet.url();
+        Helper.printChatMessage("Server resource pack url: " + url);
     }
 
     @EventHandler

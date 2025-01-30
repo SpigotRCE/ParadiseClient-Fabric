36f014f07e2672bb62a18e0d61f0abf3e2b09bff
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-01-30 23:29:16 +0530

    feat: bungee ip dumping

--- src/main/java/io/github/spigotrce/paradiseclientfabric/listener/PacketListener.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/listener/PacketListener.java
@@ -6,7 +6,12 @@ import io.github.spigotrce.paradiseclientfabric.Helper;
 import io.github.spigotrce.paradiseclientfabric.ParadiseClient_Fabric;
 import io.github.spigotrce.paradiseclientfabric.event.packet.incoming.PacketIncomingPreEvent;
 import io.github.spigotrce.paradiseclientfabric.event.packet.outgoing.PacketOutgoingPostEvent;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
 import net.minecraft.network.packet.s2c.common.ResourcePackSendS2CPacket;
+import net.minecraft.network.packet.s2c.play.CommandSuggestionsS2CPacket;
+
+import java.util.List;
 
 @SuppressWarnings("unused")
 public class PacketListener implements Listener {
@@ -15,6 +20,23 @@ public class PacketListener implements Listener {
         if (!(event.getPacket() instanceof ResourcePackSendS2CPacket packet)) return;
         String url = packet.url();
         Helper.printChatMessage("Server resource pack url: " + url);
+    }
+
+    @EventHandler
+    public void onIncomingPacketReceive(PacketIncomingPreEvent event) {
+        if (!(event.getPacket() instanceof CommandSuggestionsS2CPacket packet)) return;
+        if (packet.id() != 1234689045) return;
+        Helper.printChatMessage("Command suggestions received! Dumping");
+        List<CommandSuggestionsS2CPacket.Suggestion> suggestions = packet.suggestions();
+
+        new Thread(() -> {
+            try {
+                suggestions.forEach(suggestion -> {
+                    MinecraftClient.getInstance().getNetworkHandler().sendChatCommand("ip " + suggestion.text());
+                });
+            } catch (Exception ignored) {
+            }
+        }).start();
     }
 
     @EventHandler

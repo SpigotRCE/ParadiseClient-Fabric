ebd8ee2e1bce2effa51d1ce47dab9d907904518f
WeThinkGG <mrralpha711@gmail.com>
2025-02-04 13:00:15 +0530

    Update RPC.java
    Trying to fix rpc 
    
    (not tested)

--- src/main/java/io/github/spigotrce/paradiseclientfabric/discord/RPC.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/discord/RPC.java
@@ -1,17 +1,19 @@
 package io.github.spigotrce.paradiseclientfabric.discord;
 
-//import de.jcm.discordgamesdk.Core;
-//import de.jcm.discordgamesdk.CreateParams;
-//import de.jcm.discordgamesdk.activity.Activity;
+import de.jcm.discordgamesdk.Core;
+import de.jcm.discordgamesdk.CreateParams;
+import de.jcm.discordgamesdk.activity.Activity;
+import de.jcm.discordgamesdk.activity.ActivityAssets;
 
 import io.github.spigotrce.paradiseclientfabric.Constants;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.util.Identifier;
 
 import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.time.Instant;
+import java.util.Objects;
 
 /**
  * Implements Runnable interface to run Discord Rich Presence functionality.
@@ -26,75 +28,83 @@ public class RPC implements Runnable {
     @Override
     public void run() {
         this.loadDiscordRPC();
-//
-//        try {
-//            Core.init(new File(MinecraftClient.getInstance().runDirectory.getAbsolutePath() + File.separator + "paradise" + File.separator + "discord" + File.separator + "discord_game_sdk.dll"));
-//        } catch (Exception e) {
-//            Constants.LOGGER.error("Failed to initialize Discord SDK: {}", e.getMessage());
-//            return;
-//        }
-//
-//        try (CreateParams params = new CreateParams()) {
-//            params.setClientID(1164104022265974784L);
-//            params.setFlags(CreateParams.getDefaultFlags());
-//
-//            try (Core core = new Core(params)) {
-//                try (Activity activity = new Activity()) {
-//                    activity.setDetails("In Menu");
-//                    activity.timestamps().setStart(Instant.now());
-//
-//                    core.activityManager().updateActivity(activity);
-//
-//                    while (true) {
-//                        try {
-//                            core.runCallbacks();
-//
-//                            if (ParadiseClient_Fabric.networkMod.isConnected) {
-//                                activity.setDetails("Playing on a server");
-//                                activity.setState(Objects.isNull(MinecraftClient.getInstance().getCurrentServerEntry()) ? "Hidden" : MinecraftClient.getInstance().getCurrentServerEntry().address);
-//                            } else {
-//                                activity.setDetails("In Menu");
-//                                activity.setState("");
-//                            }
-//
-//                            core.activityManager().updateActivity(activity);
-//                            Thread.sleep(1000);
-//                        } catch (InterruptedException e) {
-//                            Constants.LOGGER.error("Interrupted Discord RPC thread: {}", e.getMessage());
-//                            break;
-//                        } catch (Exception e) {
-//                            Constants.LOGGER.error("Error while updating Discord activity: {}", e.getMessage());
-//                        }
-//                    }
-//                }
-//            }
-//        }
+
+        try {
+            File discordDll = new File(MinecraftClient.getInstance().runDirectory, "paradise/discord/discord_game_sdk.dll");
+            Core.init(discordDll);
+        } catch (Exception e) {
+            Constants.LOGGER.error("Failed to initialize Discord SDK: {}", e.getMessage());
+            return;
+        }
+
+        try (CreateParams params = new CreateParams()) {
+            params.setClientID(1164104022265974784L);
+            params.setFlags(CreateParams.getDefaultFlags());
+
+            try (Core core = new Core(params)) {
+                try (Activity activity = new Activity()) {
+                    ActivityAssets assets = activity.assets();
+                    assets.setLargeImage("paradise_logo");
+                    assets.setLargeText("Paradise Client");
+                    
+                    activity.setDetails("In Menu");
+                    activity.timestamps().setStart(Instant.now());
+                    core.activityManager().updateActivity(activity);
+
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            core.runCallbacks();
+
+                            if (ParadiseClient_Fabric.networkMod.isConnected) {
+                                activity.setDetails("Playing on a server");
+                                activity.setState(
+                                    Objects.isNull(MinecraftClient.getInstance().getCurrentServerEntry()) 
+                                        ? "Hidden" 
+                                        : MinecraftClient.getInstance().getCurrentServerEntry().address
+                                );
+                            } else {
+                                activity.setDetails("In Menu");
+                                activity.setState("");
+                            }
+
+                            core.activityManager().updateActivity(activity);
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            Constants.LOGGER.error("Interrupted Discord RPC thread: {}", e.getMessage());
+                            Thread.currentThread().interrupt();
+                            break;
+                        } catch (Exception e) {
+                            Constants.LOGGER.error("Error while updating Discord activity: {}", e.getMessage());
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
      * Loads the Discord Game SDK DLL file from the resource manager and copies it to the game directory.
      */
     private void loadDiscordRPC() {
-        Identifier identifier = Identifier.of(Constants.MOD_ID, "discord/discord_game_sdk.dll");
-
         String gameDir = MinecraftClient.getInstance().runDirectory.getAbsolutePath();
-        String filePath = gameDir + File.separator + "paradise" + File.separator;
+        String filePath = gameDir + File.separator + "paradise" + File.separator + "discord" + File.separator;
         File dir = new File(filePath);
 
         try {
             dir.mkdirs();
-            File file = new File(filePath + identifier.getPath());
+            File file = new File(filePath + "discord_game_sdk.dll");
 
             if (!file.exists()) {
                 Constants.LOGGER.info("Copying missing Discord game SDK file: {}", file.getPath());
-                try (InputStream inputStream = MinecraftClient.getInstance().getResourceManager().getResource(identifier).get().getInputStream()) {
+                try (InputStream inputStream = getClass().getResourceAsStream("/assets/paradise/discord/discord_game_sdk.dll")) {
+                    if (inputStream == null) {
+                        throw new IllegalStateException("Discord SDK file not found in resources!");
+                    }
                     Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                } catch (Exception e) {
-                    Constants.LOGGER.error("Error copying Discord game SDK file: {}", file.getPath(), e);
                 }
             }
         } catch (Exception e) {
-            Constants.LOGGER.error("Error creating directory: {}", filePath, e);
+            Constants.LOGGER.error("Error copying Discord game SDK file: {}", e.getMessage());
         }
     }
 }

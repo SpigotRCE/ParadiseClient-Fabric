365acdbb49c7cf893117a2e1fecc42f8ff8d2219
SpigotRCE <spigotrce@users.noreply.github.com>
2025-03-04 00:00:18 +0530

    config impl

--- src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/server/Main.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/server/Main.java
@@ -5,12 +5,26 @@ import io.github.spigotrce.paradiseclientfabric.chatroom.exception.UserAlreadyRe
 import io.github.spigotrce.paradiseclientfabric.chatroom.server.config.Config;
 import io.github.spigotrce.paradiseclientfabric.chatroom.server.discord.DiscordBotImpl;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-    public static Config CONFIG;
+    public static Config CONFIG = new Config(new File(System.getProperty("user.dir")).toPath());
 
     public static void main(String[] args) {
+        try {
+            CONFIG.load();
+        } catch (IOException exception) {
+            System.out.println("[ERROR] Unable to load configuration: " + exception.getMessage());
+            new ArrayList<>(
+                    Arrays.asList(exception.getStackTrace())
+            ).forEach(
+                    traceLine -> System.out.println("[ERROR] " + traceLine)
+            );
+        }
         DiscordBotImpl.startDiscordBot();
     }
 

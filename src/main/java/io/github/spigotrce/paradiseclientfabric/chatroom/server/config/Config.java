8159146d542232ec5b975171936d12691bf6d389
SpigotRCE <spigotrce@users.noreply.github.com>
2025-03-04 09:59:04 +0530

    feat: more config

--- src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/server/config/Config.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/server/config/Config.java
@@ -32,6 +32,26 @@ public class Config extends ConfigProvider {
         return getFileConfig().getString("use_haproxy");
     }
 
+    public String getDatabaseHostname() {
+        return getFileConfig().getString("database_hostname");
+    }
+
+    public String getDatabaseUsername() {
+        return getFileConfig().getString("database_username");
+    }
+
+    public String getDatabasePassword() {
+        return getFileConfig().getString("database_password");
+    }
+
+    public String getDatabaseName() {
+        return getFileConfig().getString("database_name");
+    }
+
+    public String getDatabaseConnectionParameters() {
+        return getFileConfig().getString("database-connection-parameters");
+    }
+
     @Override
     public void onReload() {
     }

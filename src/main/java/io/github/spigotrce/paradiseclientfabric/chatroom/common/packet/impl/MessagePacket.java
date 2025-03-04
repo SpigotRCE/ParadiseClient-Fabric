1bf58e5e499450c3c1e0cf62fd430150bd5aa270
SpigotRCE <spigotrce@users.noreply.github.com>
2025-03-04 11:18:20 +0530

    feat: MessagePacket

---
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/common/packet/impl/MessagePacket.java
@@ -0,0 +1,41 @@
+package io.github.spigotrce.paradiseclientfabric.chatroom.common.packet.impl;
+
+import io.github.spigotrce.paradiseclientfabric.chatroom.common.packet.Packet;
+import io.netty.buffer.ByteBuf;
+
+import java.nio.charset.Charset;
+
+public class MessagePacket extends Packet {
+    private String message;
+
+    public MessagePacket() {
+    }
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public void encode(ByteBuf buffer) {
+        buffer.writeCharSequence(message, Charset.defaultCharset());
+    }
+
+    @Override
+    public void decode(ByteBuf buffer) {
+        message = buffer.readCharSequence(buffer.readableBytes(), Charset.defaultCharset()).toString();
+    }
+
+    @Override
+    public Packet create() {
+        return new MessagePacket();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

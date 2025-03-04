25c537c787fad6420ee2baea4e014321c19048d5
SpigotRCE <spigotrce@users.noreply.github.com>
2025-03-04 10:25:26 +0530

    feat: colored logging

--- src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/Logging.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/Logging.java
@@ -6,23 +6,23 @@ import java.util.Arrays;
 public class Logging {
     public static void debug(String message) {
         // TODO: Add config option to enable/disable debugging
-        System.out.println("[DEBUG/ChatServer] " + message);
+        System.out.println("\u001B[32m[DEBUG/ChatServer] " + message + "\u001B[0m");
     }
 
     public static void info(String message) {
         System.out.println("[INFO/ChatServer] " + message);
     }
 
-    public static void earning(String message) {
-        System.out.println("[WARNING/ChatServer] " + message);
+    public static void warning(String message) {
+        System.out.println("\u001B[33m[WARNING/ChatServer] " + message + "\u001B[0m");
     }
 
     public static void warning(String message, Exception exception) {
-        System.out.println("[WARNING/ChatServer] " + message + ": " + exception.getMessage());
+        System.out.println("\u001B[33m[WARNING/ChatServer] " + message + ": " + exception.getMessage() + "\u001B[0m");
         new ArrayList<>(
                 Arrays.asList(exception.getStackTrace())
         ).forEach(
-                traceLine -> System.out.println("[WARNING/ChatServer] Stack trace ->" + traceLine)
+                traceLine -> System.out.println("\u001B[33m[WARNING/ChatServer] Stack trace ->" + traceLine + "\u001B[0m")
         );
     }
 
@@ -31,7 +31,7 @@ public class Logging {
         new ArrayList<>(
                 Arrays.asList(exception.getStackTrace())
         ).forEach(
-                traceLine -> System.out.println("[ERROR/ChatServer] Stack trace ->" + traceLine)
+                traceLine -> System.out.println("\u001B[31m[ERROR/ChatServer] Stack trace ->" + traceLine + "\u001B[0m")
         );
     }
 }

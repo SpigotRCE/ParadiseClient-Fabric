0efb09773e31bc4cda08f8b5db663c806cf15f62
SpigotRCE <spigotrce@users.noreply.github.com>
2025-03-06 12:27:22 +0530

    refactor: RSAEncryption

--- src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/common/cipher/RSAEncryption.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/common/cipher/RSAEncryption.java
@@ -1,24 +1,19 @@
 package io.github.spigotrce.paradiseclientfabric.chatroom.common.cipher;
 
 import javax.crypto.Cipher;
-import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 
 public class RSAEncryption {
-    public static String encrypt(String data, String base64PublicKey) throws Exception {
-        byte[] keyBytes = Base64.getDecoder().decode(base64PublicKey);
-        PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(keyBytes));
+    public static byte[] encrypt(byte[] data, PublicKey publicKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        return Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes()));
+        return cipher.doFinal(data);
     }
 
-    public static String decrypt(String encryptedData, PrivateKey privateKey) throws Exception {
+    public static byte[] decrypt(byte[] data, PrivateKey privateKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedData)));
+        return cipher.doFinal(data);
     }
 }

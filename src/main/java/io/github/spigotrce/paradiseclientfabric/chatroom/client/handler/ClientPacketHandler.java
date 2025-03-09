5450254880635c778d3137757ebd5f5ea3d0b384
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-03-09 11:23:19 +0530

    feat: keep alive

--- src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/client/handler/ClientPacketHandler.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/chatroom/client/handler/ClientPacketHandler.java
@@ -1,18 +1,24 @@
 package io.github.spigotrce.paradiseclientfabric.chatroom.client.handler;
 
+import io.github.spigotrce.paradiseclientfabric.Constants;
 import io.github.spigotrce.paradiseclientfabric.Helper;
 import io.github.spigotrce.paradiseclientfabric.ParadiseClient_Fabric;
 import io.github.spigotrce.paradiseclientfabric.chatroom.common.model.UserModel;
+import io.github.spigotrce.paradiseclientfabric.chatroom.common.packet.PacketRegistry;
 import io.github.spigotrce.paradiseclientfabric.chatroom.common.packet.handler.AbstractPacketHandler;
 import io.github.spigotrce.paradiseclientfabric.chatroom.common.packet.impl.DisconnectPacket;
 import io.github.spigotrce.paradiseclientfabric.chatroom.common.packet.impl.HandshakeResponsePacket;
+import io.github.spigotrce.paradiseclientfabric.chatroom.common.packet.impl.KeepAlivePacket;
 import io.github.spigotrce.paradiseclientfabric.chatroom.common.packet.impl.MessagePacket;
+import io.github.spigotrce.paradiseclientfabric.chatroom.server.Logging;
 import io.github.spigotrce.paradiseclientfabric.mod.ChatRoomMod;
 import io.netty.channel.Channel;
 
 public class ClientPacketHandler extends AbstractPacketHandler {
     private boolean isAuthenticated = false;
     private UserModel userModel;
+    public long timeSinceKeepAlive = 0;
+    public int keepAliveViolation = 0;
 
     public ClientPacketHandler(Channel channel) {
         super(channel);
@@ -28,6 +34,46 @@ public class ClientPacketHandler extends AbstractPacketHandler {
         else
             Helper.printChatMessage("[ChatRoom] Failed to authenticate: " + userModel.username());
         ParadiseClient_Fabric.CHAT_ROOM_MOD.user = userModel;
+
+        new Thread(() -> {
+            while (channel.isOpen()) {
+                try {
+                    Thread.sleep(5000);
+                    if (timeSinceKeepAlive + 5000 < System.currentTimeMillis()) {
+                        keepAliveViolation++;
+                        if (keepAliveViolation > 3) {
+                            Helper.printChatMessage("[ChatRoom] Timed out");
+                            channel.close();
+                            break;
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    Constants.LOGGER.error("Error occurred while waiting.", e);
+                    Helper.printChatMessage("[ChatRoom] Error occurred while waiting");
+                    channel.close();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            while (channel.isOpen()) {
+                try {
+                    Thread.sleep(5000);
+                    PacketRegistry.sendPacket(new KeepAlivePacket(1), channel);
+                    Helper.printChatMessage("Sending packet!");
+                } catch (InterruptedException e) {
+                    Constants.LOGGER.error("Error occurred while waiting.", e);
+                    channel.close();
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void handle(KeepAlivePacket packet) throws Exception {
+        if (!isAuthenticated)
+            throw new IllegalStateException("User not authenticated");
+        timeSinceKeepAlive = System.currentTimeMillis();
     }
 
     @Override

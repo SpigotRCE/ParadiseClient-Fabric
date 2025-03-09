6cab63cf4f0c9ade988dd26289711c0d4000fd47
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-03-09 11:40:21 +0530

    release: 1.21.4-3-15

--- src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     /**
      * The version of the mod.
      */
-    public static final String VERSION = "1.21.4-3-14";
+    public static final String VERSION = "1.21.4-3-15";
 
     /**
      * The ID of the mod.
